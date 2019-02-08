7cbcebb16ac590514b4fd01c94be04d7162f8e61
Dirk Avery <dirk.avery@gmail.com>
2019-02-08 14:24:26 -0500

    Do mocking

--- tests/test_cheese.py
+++ tests/test_cheese.py
@@ -3,11 +3,12 @@
 import car
 import drive
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import pytest
 
 class DriveTestCase(unittest.TestCase):
 
+    """
     @patch.object(drive.car.Ignition, 'start_engine', autospec=True)
     @patch.object(drive.car.Car, 'find_ignition', autospec=True)
     def test_ignition(self, mock_ignition, mock_engine):
@@ -15,6 +16,19 @@ class DriveTestCase(unittest.TestCase):
         d.go_driving()
 
         # verify
-        
+
         mock_ignition.assert_called_once()
         mock_engine.assert_called_once()
+    """
+
+    @patch.object(drive.car.Car, 'find_ignition', autospec=True)
+    def test_ignition2(self, mock_ignition):
+        engine = MagicMock()
+
+        mock_ignition.return_value.start_engine.return_value = engine
+
+        d = drive.Drive()
+        d.go_driving()
+
+        # verify
+        mock_ignition.return_value.start_engine.assert_called_once()
